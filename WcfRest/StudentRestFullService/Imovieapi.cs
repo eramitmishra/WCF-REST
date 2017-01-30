0c6f62cf18151e026e0f2c8a028b982b07fc1fd2
amitmishra <amitmishra.bst@rediffmail.com>
2017-01-30 13:41:05 +0000

    commit new rest service
    Rest service For Movie

---
+++ WcfRest/StudentRestFullService/Imovieapi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MovieRestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Imovieapi" in both code and config file together.
+    [ServiceContract]
+    public interface Imovieapi
+    {
+        /*Service at http://localhost/MovieRestFullService/movieapi.svc/GetMovieList */
+        [OperationContract]
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped,
+            Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "GetMovieList")]
+        List<Movie> getMovieList();
+    }
+}
---
+++ WcfRest/StudentRestFullService/movieapi.svc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.OleDb;
+
+namespace MovieRestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "movieapi" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select movieapi.svc or movieapi.svc.cs at the Solution Explorer and start debugging.
+    public class movieapi : Imovieapi
+    {
+        public void DoWork()
+        {
+        }
+        public List<Movie> getMovieList()
+        {
+            try
+            {
+                   string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\github\WCF-REST\WcfRest\StudentRestFullService\App_Data\moviedatabase.accdb";
+
+
+                //SqlConnection conn = new SqlConnection(connectionString);// SqlConnection(@"data source=.\SQLEXPRESS;Integrated Security=SSPI;AttachDBFilename=|DataDirectory|\dbStudent.mdf;User Instance=true");
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                conn.Open();
+                OleDbCommand com = new OleDbCommand("Select * from Movie", conn);
+                OleDbDataReader sdr = com.ExecuteReader();
+                List<Movie> lsmovie = new List<Movie>();
+                while (sdr.HasRows)
+                {
+                    Movie obmiv = new Movie();
+
+                    // get the results of each column
+                    obmiv.ID =  int.Parse(sdr["ID"] as string);
+                    obmiv.Title =  sdr["Title"] as string;
+                    obmiv.Description =  sdr["Description"] as string;
+                    obmiv.Image =  sdr["Image"] as string;
+                    obmiv.GenreId =  Convert.ToInt32(sdr["GenreId"] as string);
+                    obmiv.Director =  sdr["Director"] as string;
+                    obmiv.Writer =  sdr["Writer"] as string;
+                    obmiv.Producer =  sdr["Producer"] as string;
+                    obmiv.ReleaseDate =  Convert.ToDateTime(sdr["ReleaseDate"] as string);
+                    obmiv.Rating =  (byte)sdr["Rating"];
+                    obmiv.TrailerURI = sdr["TrailerURI"] as string;
+                    lsmovie.Add(obmiv);
+                }
+
+
+                conn.Close();
+
+
+
+
+                //using (dbMoviesEntities objMoviesDB = new dbMoviesEntities())
+                //{
+                //    //objMoviesDB.Connection.Open();
+                //    // the rest
+                // return objMoviesDB.Movies.Select(a => a).ToList();
+                //}
+
+                return lsmovie;
+                
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+
+            }
+
+        }
+
+
+    }
+}
